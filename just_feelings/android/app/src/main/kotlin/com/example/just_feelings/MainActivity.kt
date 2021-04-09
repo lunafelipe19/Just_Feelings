5ca70b8a394e997f019d50aa66b3f3e55a28b211
lunafelipe19 <felipelunacover@gmail.com>
2021-04-09 15:37:24 -0300

    commit correction

--- just_feelings/android/app/src/main/kotlin/com/example/just_feelings/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.just_feelings
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- just_feelings/lib/utils/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xFF00BFA5);
-const kPrimaryLightColor = Color(0xFFF1E6FF);
